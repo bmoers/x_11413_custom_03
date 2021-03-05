68305064789aa95a22ec930a50f1b8bec7a7e688
System Administrator <boris_moers@rcomext.com>
2021-03-05 13:49:30 +0100

    Changes by 'System Administrator' on '02 > Custom App 03 - TST US 2' -- Build #2

---
+++ sn/02 Custom App 03/Business Rule/x_11413_custom_03_pff/active_true/before/ccccccccccccccccccccccccccccccccccc/script.js
@@ -0,0 +1,14 @@
+/* 
+ * Application : 02 > Custom App 03
+ * ClassName   : sys_script
+ * Created On  : 2021-03-05 12:24:08
+ * Created By  : admin
+ * Updated On  : 2021-03-05 12:32:49
+ * Updated By  : admin
+ * URL         : /sys_script.do?sys_id=8e1ed3802ff62c10d8455aab2799b60f
+ */
+(function executeRule(current, previous /*null when async*/) {
+
+	// Add your code here
+
+})(current, previous);
